f717008f07cc24611a51c703e278ac0f74e64c6b
isaacfulcrum <isaac_cas_2000@outlook.com>
2020-12-04 19:01:32 -0600

    Security update

--- magnum/magnum/settings.py
+++ magnum/magnum/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secret_settings import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=0hxsn26=j4)4!&-ic0j%8gpn($raadqi_qo85j3v695n)ck4e'
+SECRET_KEY = secret()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
